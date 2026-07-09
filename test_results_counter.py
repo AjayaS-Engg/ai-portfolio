7f3f9ed0b40520abb84ba08985d81ec3d5efd1e1
AjayaS-Engg <294042481+AjayaS-Engg@users.noreply.github.com>
2026-07-08 18:17:43 -0700

    Refactor counter to handle any status dynamically

--- test_results_counter.py
+++ test_results_counter.py
@@ -1,16 +1,13 @@
-results = ["PASS", "FAIL", "PASS", "PASS", "FAIL","PASS","SKIP", "FAIL", "PASS", "SKIP"]
-pass_count = 0
-fail_count = 0
-skip_count = 0
-for result in results:
-    if result == "PASS":
-        pass_count += 1
-    elif result == "FAIL":
-        fail_count += 1
-    elif result == "SKIP":
-        skip_count += 1
+results = ["PASS", "FAIL", "PASS", "PASS", "FAIL","PASS","SKIP", "FAIL", "PASS", "SKIP","BLOCKED"]
+counts = {}
 
-print("Total PASS:", pass_count)
-print("Total FAIL:", fail_count)
-print("Total SKIP:", skip_count)
-print("Total results:", pass_count + fail_count + skip_count)
+for result in results:
+    if result in counts:
+        counts[result] += 1
+    else:
+        counts[result] =0+ 1
+
+print("Total PASS:", counts["PASS"])
+print("Total FAIL:", counts["FAIL"])
+print("Total SKIP:", counts["SKIP"])
+print("Total results:", sum(counts.values()))
